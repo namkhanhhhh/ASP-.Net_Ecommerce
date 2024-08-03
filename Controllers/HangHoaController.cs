94b3f584530ae665f04988f76ee8718f88778bd4
namkhanhhhh <khanhnguyennam04@gmail.com>
2024-08-03 20:39:11 +0700

    layout

---
+++ Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop_ASP_MVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShop_ASP_MVC.Data;
+
+namespace OnlineShop_ASP_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<Hshop2023Context>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("HShop"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
